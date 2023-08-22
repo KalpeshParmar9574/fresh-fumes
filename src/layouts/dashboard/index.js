71842e8790ac87d7a399d5b4f311fcdf54b6498f
Dutt Mistry <dutta.mistry@gmail.com>
2023-08-22 20:39:51 +0530

    Appbar

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useTheme, useMediaQuery, Divider, List, ListItem, ListItemButton, ListItemText, AppBar, Drawer, Container } from "@mui/material";
+import { useTheme, useMediaQuery, Divider, List, ListItem, ListItemButton, ListItemText, AppBar, Drawer, Container, Grid, Hidden } from "@mui/material";
 import Box from "@mui/material/Box";
 import MuiDrawer from "@mui/material/Drawer";
 import MuiAppBar from "@mui/material/AppBar";
@@ -25,6 +25,15 @@ const AppBarRootStyle = styled(AppBar)(({ theme }) => ({
   // zIndex: '1',
 }));
 // Define your other styled components here
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
 
 export default function MiniDrawer(props) {
   const { children,window } = props;
@@ -58,11 +67,163 @@ export default function MiniDrawer(props) {
 
   const container = window !== undefined ? () => window().document.body : undefined;
 
+   const [isDrawerOpen, setDrawerOpen] = React.useState(false);
+
+   const toggleDrawer = (open) => (event) => {
+     if (
+       event &&
+       event.type === "keydown" &&
+       (event.key === "Tab" || event.key === "Shift")
+     ) {
+       return;
+     }
+
+     setDrawerOpen(open);
+   };
+   const drawerList = ["News", "News", "News", "News", "Search", "Cart"];
+   const appbarNames = ["News", "Search", "Cart"];
+   const drawerBottomList = ["+919658741222", "Cart"];
+   const drawerContent = (
+     <div>
+       <List>
+         {drawerList?.map((item, index) => {
+           return (
+             <>
+               <ListItem button key={index}>
+                 <ListItemText
+                   primary={item}
+                   style={{ textAlign: "center", cursor: "pointer" }}
+                 />
+               </ListItem>
+               <Divider />
+             </>
+           );
+         })}
+       </List>
+       <List sx={{ marginTop: "auto" }}>
+         {drawerBottomList?.map((item, index) => {
+           return (
+             <>
+               <ListItem button key={index}>
+                 <ListItemText
+                   primary={item}
+                   style={{ textAlign: "center", cursor: "pointer" }}
+                 />
+               </ListItem>
+               <Divider />
+             </>
+           );
+         })}
+       </List>
+     </div>
+   );
+
+
   return (
     <div className="background-color">
-      <Box sx={{ display: "flex" }}>
-        <CssBaseline />
+      <CssBaseline />
+      <Box sx={{ flexGrow: 1 }}>
         <AppBarRootStyle position="fixed" component="nav">
+          {/* <AppBar position="static"> */}
+          <Toolbar>
+            <Grid container alignItems="center" justifyContent="center">
+              <Hidden smUp>
+                <Grid item xs={2}>
+                  <IconButton
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    onClick={toggleDrawer(true)}
+                  >
+                    <MenuIcon />
+                  </IconButton>
+                </Grid>
+              </Hidden>
+              <Hidden smDown>
+                <Grid item xs={8} sm={4}>
+                  <Typography
+                    variant="h6"
+                    component="div"
+                    sx={{ textAlign: "center" }}
+                  >
+                    Mobile Number
+                  </Typography>
+                </Grid>
+              </Hidden>
+              <Grid item xs={8} sm={4}>
+                <Typography
+                  variant="h6"
+                  component="div"
+                  sx={{ textAlign: "center" }}
+                >
+                  Call
+                </Typography>
+              </Grid>
+              <Hidden smDown>
+                <Grid item xs={8} sm={4}>
+                  <Typography
+                    variant="h6"
+                    component="div"
+                    sx={{ textAlign: "center" }}
+                  >
+                    Locate
+                  </Typography>
+                </Grid>
+              </Hidden>
+              <Grid item xs={2} sm={4} />
+            </Grid>
+          </Toolbar>
+          <Hidden smDown>
+            <Toolbar>
+              <Hidden xsDown={!isDrawerOpen}>
+                <Grid container justifyContent="center">
+                  {drawerList?.map((item, index) => {
+                    return (
+                      <Grid item sm={2} key={index}>
+                        <Typography
+                          variant="h6"
+                          component="div"
+                          sx={{ textAlign: "center" }}
+                        >
+                          {item}
+                        </Typography>
+                      </Grid>
+                    );
+                  })}
+                </Grid>
+              </Hidden>
+              <Grid container justifyContent="center">
+                <Hidden xsDown={!isDrawerOpen}>
+                  {appbarNames?.map((item, index) => {
+                    return (
+                      <Grid item sm={2} key={index}>
+                        <Typography
+                          variant="h6"
+                          component="div"
+                          sx={{ textAlign: "center" }}
+                        >
+                          {item}
+                        </Typography>
+                      </Grid>
+                    );
+                  })}
+                </Hidden>
+              </Grid>
+            </Toolbar>
+          </Hidden>
+        </AppBarRootStyle>
+        {/* </AppBar> */}
+        <Drawer
+          anchor="left"
+          open={isDrawerOpen}
+          onClose={toggleDrawer(false)}
+          PaperProps={{ sx: { width: 200 } }}
+          style={{ textAlign: "center" }}
+        >
+          {drawerContent}
+        </Drawer>
+      </Box>
+      {/* <AppBarRootStyle position="fixed" component="nav">
           <Container maxWidth="xl">
             <Toolbar>
               <IconButton
@@ -90,9 +251,8 @@ export default function MiniDrawer(props) {
               </Box>
             </Toolbar>
           </Container>
-        </AppBarRootStyle>
-        <CssBaseline />
-        <Box component="nav">
+        </AppBarRootStyle> */}
+      {/* <Box component="nav">
           <Drawer
             container={container}
             variant="temporary"
@@ -111,24 +271,21 @@ export default function MiniDrawer(props) {
           >
             {drawer}
           </Drawer>
-        </Box>
-        {/* <ScrollTop {...props}>
+        </Box> */}
+      {/* <ScrollTop {...props}>
           <Fab size="small" aria-label="scroll back to top">
             <KeyboardArrowUpIcon />
           </Fab>
         </ScrollTop> */}
-        <Box
-          component="main"
-          sx={{
-            flexGrow: 1,
-            marginTop: isSmallScreen ? "2rem" : "1rem",
-            marginBottom: "2rem",
-          }}
-          id="back-to-top-anchor"
-        >
-          {/* <DrawerHeader /> */}
-          {children}
-        </Box>
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, marginTop: "3.5rem", marginBottom: "2rem" }}
+      >
+       <Container maxWidth="xl">
+        <DrawerHeader />
+        {children}
+        {/* <Chat /> */}
+        </Container>
       </Box>
     </div>
   );
